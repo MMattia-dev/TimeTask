bbc35cf8c147b6162dd8f36e7ef7950018d64ff9
MMattia-dev <kromolski@gmail.com>
2023-06-30 19:56:21 +0200

    30.06.2023 (1)

--- Controllers/TimesController.cs
+++ Controllers/TimesController.cs
@@ -167,11 +167,14 @@ namespace TimeTask.Controllers
         {
             ViewBag.Departments = _context.Department;
             ViewBag.Workers = _context.Workers2;
+            ViewBag.Holiday = _context.Holiday;
 
             return _context.Time != null ?
                           View(await _context.Time.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Time'  is null.");
         }
+
+
 
 
     }
