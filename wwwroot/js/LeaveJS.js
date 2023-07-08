76682b927f0281738e28336e43777ab53042648a
MMattia-dev <kromolski@gmail.com>
2023-07-08 12:18:39 +0200

    `

--- wwwroot/js/LeaveJS.js
+++ wwwroot/js/LeaveJS.js
@@ -29,6 +29,10 @@ function generateCalendar()
     //const date = new Date();
     //date.setDate(1); //ustaw pierwszy dzień miesiąca
 
+    let oUfnFiNPmXnNjzu = document.getElementById('oUfnFiNPmXnNjzu');
+    let workerID_ = oUfnFiNPmXnNjzu.options[oUfnFiNPmXnNjzu.selectedIndex].value;
+
+
     let yearSelect = document.getElementById('iHCBwRzOLpgGYQG');
     let year = yearSelect.options[yearSelect.selectedIndex].value;
     let monthSelect = document.getElementById('IZdWjCoFNPZaIaP');
@@ -66,6 +70,48 @@ function generateCalendar()
     }
 
     
+    let dykKoaHBFtTPjlK = document.querySelectorAll('.dykKoaHBFtTPjlK');
+    //for (let i = 0; i < dykKoaHBFtTPjlK.length; i++) 
+    //{
+    //    let date = dykKoaHBFtTPjlK[i].id;
+    //    for (let j = 0; j < model_t.length; j++) 
+    //    {
+    //        //urlopy
+    //        if (model_t[j].Enter == null && model_t[j].Exit == null && workerID_ == model_t[j].WorkerID) {
+    //            if (model_t[j].LeaveDate.split('T')[0] == date) {
+    //                //console.log(model_t[j].LeaveID);
+    //                for (let k = 0; k < model_l.length; k++) {
+    //                    if (model_l[k].Id == model_t[j].LeaveID) {
+    //                        let name = model_l[k].Name;
+    //                        let description = model_l[k].Description;
+    //                        dykKoaHBFtTPjlK.innerHTML += `<div class="PXHhlPBPzXQFpVg">`
+    //                            + ``
+    //                        + `</div>`;
+                            
+    //                        console.log(model_l[k].Id);
+
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    for (let i = 0; i < dykKoaHBFtTPjlK.length; i++) {
+        //workerID_
+        let date = dykKoaHBFtTPjlK[i].id;
+        for (let j = 0; j < model_t.length; j++) {
+            if (model_t[j].WorkerID == workerID_ && model_t[j].Enter == null && model_t[j].Exit == null) {
+                if (model_t[j].LeaveDate.split('T')[0] == date) {
+                    //console.log(date);
+                }
+            }
+        }
+    }
+    //console.log(workerID_);
+
+
+
 
     //let days = document.querySelector('.days_');
 
@@ -136,7 +182,7 @@ function generateCalendar()
 
 
 };
-generateCalendar();
+
 
 
 function FFkdMqNnTDbWkXb()
@@ -182,32 +228,158 @@ function YDTyNWHkvmuAiCQ()
 function dWVTVhqEBjJCURf()
 {
     $('#xBuYErAxrbdvwoP').children().show();
+    generateCalendar();
 };
 
 
+$('#ttGSoqUHUjOErnf').on('click', function ()
+{
+    $('#KYZGriDIsqNJRxr').fadeOut(200);
+});
+
+$('.pyyxmssXgPCWuUc').on('click', function ()
+{
+    $('#KYZGriDIsqNJRxr').fadeIn(200);
+});
+
+$('#HvZxXypLRxeRXCo').on('change', function ()
+{
+    document.getElementById('dFiioMzmTCjjcWp').innerHTML = ``;
+
+    for (let i = 0; i < model_l.length; i++)
+    {
+        if (model_l[i].Name == this.options[this.selectedIndex].text)
+        {
+            //if (model_l[i].Description != null)
+            //{
+            //    $('#dFiioMzmTCjjcWp').removeClass('fdjtgOVkxlRqfDM');
+            //    document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">` + model_l[i].Description + `</option>`;
+            //}
+            //else {
+            //    $('#dFiioMzmTCjjcWp').addClass('fdjtgOVkxlRqfDM');
+            //    //document.getElementById('dFiioMzmTCjjcWp').innerHTML = `<option></option>`;
+            //}
 
 
-//$(document).ready(function ()
-//{
-//    if (sessionStorage.getItem('iJZfSckTVemIXEE') != null)
-//    {
-//        //let e = document.getElementById('aFoQOFiXPQobjPX');
-//        //let e2 = e.options[e.selectedIndex].value;
-//        //document.getElementById('aFoQOFiXPQobjPX').selectedIndex = parseInt(sessionStorage.getItem('iJZfSckTVemIXEE'));
-//        $('#aFoQOFiXPQobjPX option').each(function ()
-//        {
-//            //if ($(this).css('display') != 'none')
-//            //{
-//            //    $(this).prop("selected", true);
-//            //    return false;
-//            //}
-//            if ($(this).val() == sessionStorage.getItem('iJZfSckTVemIXEE')) {
-//                $(this).prop("selected", true);
-//                return false;
-//            }
-//        })
-//    }
-//});
+            //document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">` + model_l[i].Description + `</option>`;
+            if (model_l[i].Description != null)
+            {
+                $('#dFiioMzmTCjjcWp').removeClass('fdjtgOVkxlRqfDM');
+                document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">` + model_l[i].Description + `</option>`;
+            }
+            else
+            {
+                $('#dFiioMzmTCjjcWp').addClass('fdjtgOVkxlRqfDM');
+                document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">-</option>`;
+            }
 
 
 
+        }
+    }
+
+    sortSelect(document.getElementById('dFiioMzmTCjjcWp'));
+
+});
+
+$('#HvZxXypLRxeRXCo').trigger('change');
+
+$('#JTgCvImoJEyzGux').on('click', function ()
+{
+    var days = [];
+    var toRemove = [];
+
+    let oUfnFiNPmXnNjzu = document.getElementById('oUfnFiNPmXnNjzu');
+    let workerID_ = oUfnFiNPmXnNjzu.options[oUfnFiNPmXnNjzu.selectedIndex].value;
+    let enter_ = null;
+    let exit_ = null;
+    let dFiioMzmTCjjcWp = document.getElementById('dFiioMzmTCjjcWp');
+    let leaveID_ = dFiioMzmTCjjcWp.options[dFiioMzmTCjjcWp.selectedIndex].value;
+
+    let od_ = document.getElementById('OYRMUMzpHRsooyI').value;
+    let do_ = document.getElementById('tyONXYuOELdPoLh').value;
+
+    if (od_ != '' && do_ != '') 
+    {
+        days = getDatesInRange(new Date(od_), new Date(do_));
+        for (let i = 0; i < days.length; i++) 
+        {
+            let day = new Date(days[i]);
+
+            for (let j = 0; j < model_l.length; j++) 
+            {
+                if (model_l[j].Id == leaveID_)
+                {
+
+                    if (!model_l[j].IfWeekends)
+                    {
+                        //console.log('bez weekendu');
+                        if (isWeekend(day)) 
+                        {
+                            toRemove.push(day);
+                        }
+                    }
+
+                    if (!model_l[j].IfHolidays)
+                    {
+                        //console.log('bez świąt');
+                        for (let k = 0; k < model_h.length; k++) 
+                        {
+                            if (new Date(days[i]).toISOString().split('T')[0] == model_h[k].Date.split('T')[0]) 
+                            {
+                                toRemove.push(day);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //console.log(days);
+    //console.log(toRemove);
+
+    var leaveDays = days.filter(function (e, index)
+    {
+        return (!toRemove.some(d => +d === +e));
+    });
+
+    //console.log(leaveDays);
+
+    for (let i = 0; i < leaveDays.length; i++)
+    {
+        //let date = new Date(leaveDays[i]);
+        //console.log(date);
+        let date = leaveDays[i].toISOString().split('T')[0];
+
+        $.ajax({
+            type: 'POST',
+            url: '/Times/AddLeave',
+            async: false,
+            data: {
+                workerID: workerID_,
+                enter: null,
+                exit: null,
+                leaveID: leaveID_,
+                leaveDate: date
+            },
+            success: function (response)
+            {
+                location.reload();
+            },
+            error: function (xhr, status, error)
+            {
+                console.log('Error adding column value:', error);
+            }
+        });
+    }
+
+
+});
+
+$('#iHCBwRzOLpgGYQG').on('change', function ()
+{
+    generateCalendar();
+});
+
+generateCalendar();
