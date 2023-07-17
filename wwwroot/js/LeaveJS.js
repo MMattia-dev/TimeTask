7037ad71de1f8d12743e3b199d42e671d6dcd097
MMattia-dev <kromolski@gmail.com>
2023-07-17 13:57:32 +0200

    17.07.2023

--- wwwroot/js/LeaveJS.js
+++ wwwroot/js/LeaveJS.js
@@ -147,7 +147,7 @@ function generateCalendar()
                 $(PXHhlPBPzXQFpVg[j]).hide();
             }
 
-            dykKoaHBFtTPjlK[i].innerHTML += `<div class="TYIUWPkeSfoEFoi">`
+            dykKoaHBFtTPjlK[i].innerHTML += `<div class="TYIUWPkeSfoEFoi" onclick="yKZSDGYyOfLkUoB(this)">`
                 + `<span>` + PXHhlPBPzXQFpVg.length + `</span>`
                 + `<span>pracowników ma zapisany urlop w tym dniu.</span>`
                 //+ `<span>(kliknij, aby zobaczyć szczegóły)</span>`
@@ -229,7 +229,50 @@ function generateCalendar()
 
 };
 
+function yKZSDGYyOfLkUoB(t) {
+    //let hasChild = document.body.querySelector('#RHJIQRivZsUcFkm');
+    //if (hasChild != null) 
+    //{
+    //    $(hasChild).remove();
+    //}
+    
+    //let html_ = `<div id="RHJIQRivZsUcFkm" class="pGKcZvErUB">`
+    //    + `<div class="svTlrqbGxnTrkkq">`
+    //    + `<div class="iEazXcrnGzwjSNB"><svg onclick="NWEuFXskeXpBGBa()" viewBox="0 0 469.404 469.404" width="12" height="12"><path d="M310.4,235.083L459.88,85.527c12.545-12.546,12.545-32.972,0-45.671L429.433,9.409c-12.547-12.546-32.971-12.546-45.67,0L234.282,158.967L85.642,10.327c-12.546-12.546-32.972-12.546-45.67,0L9.524,40.774c-12.546,12.546-12.546,32.972,0,45.671l148.64,148.639L9.678,383.495c-12.546,12.546-12.546,32.971,0,45.67l30.447,30.447c12.546,12.546,32.972,12.546,45.67,0l148.487-148.41l148.792,148.793c12.547,12.546,32.973,12.546,45.67,0l30.447-30.447c12.547-12.546,12.547-32.972,0-45.671L310.4,235.083z"></path></svg></div>`
+    //    + `<div class="efUljXvyQujgoTu">`
 
+    //    + `</div>`
+    //    + `</div>`
+    //    + `</div>`;
+
+    
+
+    //document.body.innerHTML += html_;
+
+    //$('#RHJIQRivZsUcFkm').css({ 'opacity': '0' });
+    
+    document.querySelector('.efUljXvyQujgoTu').innerHTML = '';
+    //let spans = '';
+    //let spans = [];
+    let PXHhlPBPzXQFpVg = $(t).parent().children();
+    for (let i = 0; i < PXHhlPBPzXQFpVg.length; i++) 
+    {
+        if ($(PXHhlPBPzXQFpVg[i]).hasClass('PXHhlPBPzXQFpVg') && PXHhlPBPzXQFpVg[i].style.display == 'none') 
+        {
+            //spans = PXHhlPBPzXQFpVg[i].innerHTML;   
+            //spans.push(PXHhlPBPzXQFpVg[i].innerHTML);
+            document.querySelector('.efUljXvyQujgoTu').innerHTML += `<div>` + PXHhlPBPzXQFpVg[i].innerHTML + `</div>`;
+        }
+    }
+    //document.querySelector('.efUljXvyQujgoTu').innerHTML = spans;
+    //console.log(spans);
+
+    $('#RHJIQRivZsUcFkm').fadeIn(200);
+};
+
+function NWEuFXskeXpBGBa() { 
+    $('#RHJIQRivZsUcFkm').fadeOut(200);
+};
 
 function FFkdMqNnTDbWkXb()
 {
