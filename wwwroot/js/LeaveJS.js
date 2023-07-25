f8003fe0d1ab42a3111f977a65ae1e61e3048df6
MMattia-dev <kromolski@gmail.com>
2023-07-25 11:35:47 +0200

    1

--- wwwroot/js/LeaveJS.js
+++ wwwroot/js/LeaveJS.js
@@ -26,11 +26,6 @@ function getLang()
 
 function generateCalendar()
 {
-    //const date = new Date();
-    //date.setDate(1); //ustaw pierwszy dzień miesiąca
-    //$('.loader_div').fadeOut(200);
-
-
     //department
     let aFoQOFiXPQobjPX = document.getElementById('aFoQOFiXPQobjPX');
     let departmentID_ = aFoQOFiXPQobjPX.options[aFoQOFiXPQobjPX.selectedIndex].value;
@@ -40,46 +35,6 @@ function generateCalendar()
     let oUfnFiNPmXnNjzu = document.getElementById('oUfnFiNPmXnNjzu');
     let workerID_ = oUfnFiNPmXnNjzu.options[oUfnFiNPmXnNjzu.selectedIndex].value;
     //
-
-
-
-    
-    
-
-
-    //let GSYLLXKlArEDwFF = sessionStorage.getItem('GSYLLXKlArEDwFF');
-    //if (GSYLLXKlArEDwFF == null)
-    //{
-    //    sessionStorage.setItem('GSYLLXKlArEDwFF', aFoQOFiXPQobjPX.options[aFoQOFiXPQobjPX.selectedIndex].value);
-    //}
-    //else {
-    //    console.log(sessionStorage.getItem('GSYLLXKlArEDwFF'));
-    //}
-
-    //let GSYLLXKlArEDwFF = sessionStorage.getItem('GSYLLXKlArEDwFF');
-    //if (GSYLLXKlArEDwFF != null)
-    //{
-    //    aFoQOFiXPQobjPX.value = GSYLLXKlArEDwFF;
-    //    //console.log(GSYLLXKlArEDwFF);
-    //}
-
-
-
-
-
-
-
-    //let PgLKgYMAvonpLlF = sessionStorage.getItem('PgLKgYMAvonpLlF');
-    //let PgLKgYMAvonpLlF_ = sessionStorage.getItem('PgLKgYMAvonpLlF_');
-    //if (PgLKgYMAvonpLlF != '' && PgLKgYMAvonpLlF_ != '')
-    //{
-    //    console.log(PgLKgYMAvonpLlF_ + ' department');
-    //    console.log(PgLKgYMAvonpLlF + ' workerID');
-    //}
-
-
-
-
 
 
 
@@ -93,10 +48,6 @@ function generateCalendar()
     }
 
 
-
-
-
-    
 
     let yearSelect = document.getElementById('iHCBwRzOLpgGYQG');
     let year = yearSelect.options[yearSelect.selectedIndex].value;
@@ -141,7 +92,7 @@ function generateCalendar()
         let date = dykKoaHBFtTPjlK[i].id;
         for (let j = 0; j < model_t.length; j++) 
         {
-            if (model_t[j].WorkerID == workerID_ && model_t[j].Enter == null && model_t[j].Exit == null)
+            if (model_t[j].Enter == null && model_t[j].Exit == null)
             {
                 $('.pyyxmssXgPCWuUc').removeClass('fNPXdDDFqqbVOkt');
 
@@ -154,28 +105,31 @@ function generateCalendar()
                             let name = model_l[k].Name;
                             let description = model_l[k].Description;
                             
-                            if (description != null) 
-                            {
-                                dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" id="` + model_t[j].Id + `">`
-                                    + `<span>` + name + `</span>`
-                                    + `<span>(` + description + `)</span>`
-                                    + `</div>`;
+                            for (let x = 0; x < model_w.length; x++) {
+                                if (model_t[j].WorkerID == model_w[x].Id && departmentID_ == model_w[x].DepartmentID) {
+                                    if (description != null) 
+                                    {
+                                        dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" onclick="yKZSDGYyOfLkUoB(this)" id="` + model_t[j].Id + `">`
+                                            + `<span>` + model_w[x].Surname + ` ` + model_w[x].Name + `</span>`
+                                            + `<span>` + name + `</span>`
+                                            + `<span>(` + description + `)</span>`
+                                            + `</div>`;
+                                    }
+                                    else 
+                                    {
+                                        dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" onclick="yKZSDGYyOfLkUoB(this)" id="` + model_t[j].Id + `">`
+                                            + `<span>` + model_w[x].Surname + ` ` + model_w[x].Name + `</span>`
+                                            + `<span>` + name + `</span>`
+                                            + `</div>`;
+                                    }
+                                }
                             }
-                            else 
-                            {
-                                dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" id="` + model_t[j].Id + `">`
-                                    + `<span>` + name + `</span>`
-                                    + `</div>`;
-                            }
-
                         }
                     }
                 }
             }
             else if (workerID_ == 'everyone' && model_t[j].Enter == null && model_t[j].Exit == null) 
             {
-                //$('.pyyxmssXgPCWuUc').addClass('fNPXdDDFqqbVOkt'); //enable
-
                 if (model_t[j].LeaveDate.split('T')[0] == date) 
                 {
                     for (let k = 0; k < model_l.length; k++)
@@ -193,7 +147,7 @@ function generateCalendar()
                                         
                                         if (description != null) 
                                         {
-                                            dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" id="` + model_t[j].Id + `">`
+                                            dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" onclick="yKZSDGYyOfLkUoB(this)" id="` + model_t[j].Id + `">`
                                                 + `<span>` + abc[m].innerHTML + `</span>`
                                                 + `<span>` + name + `</span>`
                                                 + `<span>(` + description + `)</span>`
@@ -201,7 +155,7 @@ function generateCalendar()
                                         }
                                         else 
                                         {
-                                            dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" id="` + model_t[j].Id + `">`
+                                            dykKoaHBFtTPjlK[i].innerHTML += `<div class="PXHhlPBPzXQFpVg" onclick="yKZSDGYyOfLkUoB(this)" id="` + model_t[j].Id + `">`
                                                 + `<span>` + abc[m].innerHTML + `</span>`
                                                 + `<span>` + name + `</span>`
                                                 + `</div>`;
@@ -230,83 +184,9 @@ function generateCalendar()
             dykKoaHBFtTPjlK[i].innerHTML += `<div class="TYIUWPkeSfoEFoi" onclick="yKZSDGYyOfLkUoB(this)">`
                 + `<span>` + PXHhlPBPzXQFpVg.length + `</span>`
                 + `<span>pracowników ma zapisany urlop w tym dniu.</span>`
-                //+ `<span>(kliknij, aby zobaczyć szczegóły)</span>`
             + `</div>`;
         }
     }
-    
-
-
-
-
-    //let days = document.querySelector('.days_');
-
-    //let divs = '';
-
-    ////
-    //const prevLastDay = new Date(
-    //    year,
-    //    month,
-    //    0
-    //).getDate();
-    ////
-
-    //const firstDayIndex = new Date(year, month, 1).getDay(); //pierwszy dzień miesiąca
-
-    ////
-    //const lastDayIndex = new Date(
-    //    year,
-    //    month + 1,
-    //    0
-    //).getDay();
-    ////
-
-    //const nextDays = 7 - lastDayIndex - 0;
-
-    ////dodaj dni poprzedniego miesiąca
-    //if (firstDayIndex == 0)
-    //{
-    //    for (let x = 7; x > 1; x--)
-    //    {
-    //        divs += `<div class="prev-date2">${prevLastDay - x + 2}</div>`;
-    //    }
-    //}
-    //else
-    //{
-    //    for (let x = firstDayIndex; x > 1; x--)
-    //    {
-    //        divs += `<div class="prev-date2">${prevLastDay - x + 2}</div>`;
-    //    }
-    //}
-    ////
-
-    ////dodaj dni miesiąca
-    //for (let k = 1; k <= daysLength; k++)
-    //{
-    //    let newMonth = month + 1;
-    //    newMonth = padWithLeadingZeros(newMonth, 2);
-
-    //    let newDay = k;
-    //    newDay = padWithLeadingZeros(newDay, 2);
-
-    //    divs += `<div id="` + year + `.` + newMonth + `.` + newDay + `"><span>${k}</span></div>`;
-    //}
-    ////
-
-    ////dodaj dni następnego miesiąca
-    //if (nextDays != 7)
-    //{
-    //    for (let x = 1; x <= nextDays; x++)
-    //    {
-    //        divs += `<div class="next-date2">${x}</div>`;
-    //    }
-    //}
-    ////
-
-    ////days.innerHTML = divs;
-    //document.querySelector('.days_').innerHTML = divs;
-
-
 };
 
 function yKZSDGYyOfLkUoB(t) {
@@ -315,13 +195,29 @@ function yKZSDGYyOfLkUoB(t) {
     let PXHhlPBPzXQFpVg = $(t).parent().children();
     for (let i = 0; i < PXHhlPBPzXQFpVg.length; i++) 
     {
-        if ($(PXHhlPBPzXQFpVg[i]).hasClass('PXHhlPBPzXQFpVg') && PXHhlPBPzXQFpVg[i].style.display == 'none') 
+        if ($(PXHhlPBPzXQFpVg[i]).hasClass('PXHhlPBPzXQFpVg')) 
         {
             document.querySelector('.efUljXvyQujgoTu').innerHTML += `<div class="iLPhCPQLeZriLNQ"><div class="ctIBgsayAyonpQK">` + PXHhlPBPzXQFpVg[i].innerHTML + `</div><div class="jLWszVjseUnWpuM">`
-                + `<div><a title="Szczegóły" id="LlkaLZEbAcWBnYK"><svg viewBox="0 0 1920 1920" height="18" width="18"><path d="M1229.93 594.767c36.644 37.975 50.015 91.328 43.72 142.909-9.128 74.877-30.737 144.983-56.093 215.657-27.129 75.623-54.66 151.09-82.332 226.512-44.263 120.685-88.874 241.237-132.65 362.1-10.877 30.018-18.635 62.072-21.732 93.784-3.376 34.532 21.462 51.526 52.648 36.203 24.977-12.278 49.288-28.992 68.845-48.768 31.952-32.31 63.766-64.776 94.805-97.98 15.515-16.605 30.86-33.397 45.912-50.438 11.993-13.583 24.318-34.02 40.779-42.28 31.17-15.642 55.226 22.846 49.582 49.794-5.39 25.773-23.135 48.383-39.462 68.957l-1.123 1.416a1559.53 1559.53 0 0 0-4.43 5.6c-54.87 69.795-115.043 137.088-183.307 193.977-67.103 55.77-141.607 103.216-223.428 133.98-26.65 10.016-53.957 18.253-81.713 24.563-53.585 12.192-112.798 11.283-167.56 3.333-40.151-5.828-76.246-31.44-93.264-68.707-29.544-64.698-8.98-144.595 6.295-210.45 18.712-80.625 46.8-157.388 75.493-234.619l2.18-5.867 1.092-2.934 2.182-5.87 2.182-5.873c33.254-89.517 67.436-178.676 101.727-267.797 31.294-81.296 62.72-162.537 93.69-243.95 2.364-6.216 5.004-12.389 7.669-18.558l1-2.313c6.835-15.806 13.631-31.617 16.176-48.092 6.109-39.537-22.406-74.738-61.985-51.947-68.42 39.4-119.656 97.992-170.437 156.944l-6.175 7.17c-15.78 18.323-31.582 36.607-47.908 54.286-16.089 17.43-35.243 39.04-62.907 19.07-29.521-21.308-20.765-48.637-3.987-71.785 93.18-128.58 205.056-248.86 350.86-316.783 60.932-28.386 146.113-57.285 225.882-58.233 59.802-.707 116.561 14.29 157.774 56.99Zm92.038-579.94c76.703 29.846 118.04 96.533 118.032 190.417-.008 169.189-182.758 284.908-335.53 212.455-78.956-37.446-117.358-126.202-98.219-227.002 26.494-139.598 183.78-227.203 315.717-175.87Z"></path></svg></a></div>`
-                + `<div><a title="Edytuj" id="tkHxbToRJDFsHnu"><svg viewBox="0 0 512 512" height="14" width="14"><path d="M494.56,55.774l-38.344-38.328c-23.253-23.262-60.965-23.253-84.226,0l-35.878,35.878l122.563,122.563l35.886-35.878C517.814,116.747,517.814,79.044,494.56,55.774z"></path><polygon points="0,389.435 0,511.998 122.571,511.998 425.246,209.314 302.691,86.751"></polygon></svg></a></div>`
-                + `<div><a title="Usuń" id="dDqbkhfWeyiaoZu"><svg viewBox="0 0 469.404 469.404" height="14" width="14"><path d="M310.4,235.083L459.88,85.527c12.545-12.546,12.545-32.972,0-45.671L429.433,9.409c-12.547-12.546-32.971-12.546-45.67,0L234.282,158.967L85.642,10.327c-12.546-12.546-32.972-12.546-45.67,0L9.524,40.774c-12.546,12.546-12.546,32.972,0,45.671l148.64,148.639L9.678,383.495c-12.546,12.546-12.546,32.971,0,45.67l30.447,30.447c12.546,12.546,32.972,12.546,45.67,0l148.487-148.41l148.792,148.793c12.547,12.546,32.973,12.546,45.67,0l30.447-30.447c12.547-12.546,12.547-32.972,0-45.671L310.4,235.083z"></path></svg></a></div>`
-                +`</div></div>`;
+                //+ `<div><a title="Szczegóły" id="LlkaLZEbAcWBnYK"><svg viewBox="0 0 1920 1920" height="18" width="18"><path d="M1229.93 594.767c36.644 37.975 50.015 91.328 43.72 142.909-9.128 74.877-30.737 144.983-56.093 215.657-27.129 75.623-54.66 151.09-82.332 226.512-44.263 120.685-88.874 241.237-132.65 362.1-10.877 30.018-18.635 62.072-21.732 93.784-3.376 34.532 21.462 51.526 52.648 36.203 24.977-12.278 49.288-28.992 68.845-48.768 31.952-32.31 63.766-64.776 94.805-97.98 15.515-16.605 30.86-33.397 45.912-50.438 11.993-13.583 24.318-34.02 40.779-42.28 31.17-15.642 55.226 22.846 49.582 49.794-5.39 25.773-23.135 48.383-39.462 68.957l-1.123 1.416a1559.53 1559.53 0 0 0-4.43 5.6c-54.87 69.795-115.043 137.088-183.307 193.977-67.103 55.77-141.607 103.216-223.428 133.98-26.65 10.016-53.957 18.253-81.713 24.563-53.585 12.192-112.798 11.283-167.56 3.333-40.151-5.828-76.246-31.44-93.264-68.707-29.544-64.698-8.98-144.595 6.295-210.45 18.712-80.625 46.8-157.388 75.493-234.619l2.18-5.867 1.092-2.934 2.182-5.87 2.182-5.873c33.254-89.517 67.436-178.676 101.727-267.797 31.294-81.296 62.72-162.537 93.69-243.95 2.364-6.216 5.004-12.389 7.669-18.558l1-2.313c6.835-15.806 13.631-31.617 16.176-48.092 6.109-39.537-22.406-74.738-61.985-51.947-68.42 39.4-119.656 97.992-170.437 156.944l-6.175 7.17c-15.78 18.323-31.582 36.607-47.908 54.286-16.089 17.43-35.243 39.04-62.907 19.07-29.521-21.308-20.765-48.637-3.987-71.785 93.18-128.58 205.056-248.86 350.86-316.783 60.932-28.386 146.113-57.285 225.882-58.233 59.802-.707 116.561 14.29 157.774 56.99Zm92.038-579.94c76.703 29.846 118.04 96.533 118.032 190.417-.008 169.189-182.758 284.908-335.53 212.455-78.956-37.446-117.358-126.202-98.219-227.002 26.494-139.598 183.78-227.203 315.717-175.87Z"></path></svg></a></div>`
+                + `<div><a title="Edytuj" id="tkHxbToRJDFsHnu" BOBxrOhlBwoCJjS="` + PXHhlPBPzXQFpVg[i].id + `"><svg viewBox="0 0 512 512" height="16" width="16"><path d="M494.56,55.774l-38.344-38.328c-23.253-23.262-60.965-23.253-84.226,0l-35.878,35.878l122.563,122.563l35.886-35.878C517.814,116.747,517.814,79.044,494.56,55.774z"></path><polygon points="0,389.435 0,511.998 122.571,511.998 425.246,209.314 302.691,86.751"></polygon></svg></a></div>`
+                + `<div><a title="Usuń" id="dDqbkhfWeyiaoZu" naUqxVzzjmhDYeI="` + PXHhlPBPzXQFpVg[i].id + `"><svg viewBox="0 0 24 24" height="20" width="20"><path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z"></path></svg></a></div>`
+                + `</div></div>`;
+
+
+            //console.log(PXHhlPBPzXQFpVg[i].id);
+            for (let j = 0; j < model_t.length; j++) {
+                if (PXHhlPBPzXQFpVg[i].Id == model_t[j].Id) {
+                    let workerID = model_t[j].WorkerID;
+                    let leaveID = model_t[j].LeaveID;
+                    let leaveDate = model_t[j].LeaveDate;
+
+                    
+
+                }
+                console.log(model_t[j].LeaveDate);
+                
+            }
+
         }
     }
 
@@ -334,38 +230,6 @@ function NWEuFXskeXpBGBa() {
 
 function FFkdMqNnTDbWkXb()
 {
-    //let e = document.getElementById('aFoQOFiXPQobjPX');
-    //let e2 = e.options[e.selectedIndex].value;
-
-    //let select2 = $('#oUfnFiNPmXnNjzu').children();
-    //for (let i = 0; i < select2.length; i++)
-    //{
-    //    if (e2 == select2[i].id || select2[i].value == 'everyone')
-    //    {
-    //        $(select2[i]).show();
-    //    }
-    //    else
-    //    {
-    //        $(select2[i]).hide();
-    //    }
-    //}
-
-    //$('#oUfnFiNPmXnNjzu option').each(function ()
-    //{
-    //    if ($(this).css('display') != 'none')
-    //    {
-    //        $(this).prop("selected", true);
-    //        return false;
-    //    }
-    //})
-
-
-
-    //oUfnFiNPmXnNjzu
-    //sessionStorage.setItem('PgLKgYMAvonpLlF', document.getElementById('oUfnFiNPmXnNjzu').value); //workerID
-    //sessionStorage.setItem('PgLKgYMAvonpLlF_', document.getElementById('aFoQOFiXPQobjPX').value); //departmentID
-
-
     document.getElementById('oUfnFiNPmXnNjzu').innerHTML = '';
 
     let aFoQOFiXPQobjPX = document.getElementById('aFoQOFiXPQobjPX');
@@ -376,7 +240,7 @@ function FFkdMqNnTDbWkXb()
             document.getElementById('oUfnFiNPmXnNjzu').innerHTML += `<option value="` + model_w[i].Id + `" id="` + model_w[i].DepartmentID + `">` + model_w[i].Surname + ` ` + model_w[i].Name + `</option>`;
         }
     }
-    document.getElementById('oUfnFiNPmXnNjzu').innerHTML += `<option value="everyone">Wszyscy w wybranym dziale</option>`;
+    document.getElementById('oUfnFiNPmXnNjzu').innerHTML += `<option value="everyone">Wszyscy</option>`;
     
     generateCalendar();
 };
@@ -389,13 +253,8 @@ function YDTyNWHkvmuAiCQ()
 
 function dWVTVhqEBjJCURf(t)
 {
-    //sessionStorage.setItem('PgLKgYMAvonpLlF', document.getElementById('oUfnFiNPmXnNjzu').value); //workerID
-    //sessionStorage.setItem('PgLKgYMAvonpLlF_', document.getElementById('aFoQOFiXPQobjPX').value); //departmentID
-    
     $('#xBuYErAxrbdvwoP').children().show();
     generateCalendar();
-
-    
 };
 
 
@@ -460,18 +319,6 @@ $('#HvZxXypLRxeRXCo').on('change', function ()
     {
         if (model_l[i].Name == this.options[this.selectedIndex].text)
         {
-            //if (model_l[i].Description != null)
-            //{
-            //    $('#dFiioMzmTCjjcWp').removeClass('fdjtgOVkxlRqfDM');
-            //    document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">` + model_l[i].Description + `</option>`;
-            //}
-            //else {
-            //    $('#dFiioMzmTCjjcWp').addClass('fdjtgOVkxlRqfDM');
-            //    //document.getElementById('dFiioMzmTCjjcWp').innerHTML = `<option></option>`;
-            //}
-
-
-            //document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">` + model_l[i].Description + `</option>`;
             if (model_l[i].Description != null)
             {
                 $('#dFiioMzmTCjjcWp').removeClass('fdjtgOVkxlRqfDM');
@@ -482,14 +329,10 @@ $('#HvZxXypLRxeRXCo').on('change', function ()
                 $('#dFiioMzmTCjjcWp').addClass('fdjtgOVkxlRqfDM');
                 document.getElementById('dFiioMzmTCjjcWp').innerHTML += `<option value="` + model_l[i].Id + `">-</option>`;
             }
-
-
-
         }
     }
 
     sortSelect(document.getElementById('dFiioMzmTCjjcWp'));
-
 });
 
 $('#HvZxXypLRxeRXCo').trigger('change');
@@ -499,7 +342,6 @@ $('#JTgCvImoJEyzGux').on('click', function ()
     var days = [];
     var toRemove = [];
 
-    //let oUfnFiNPmXnNjzu = document.getElementById('oUfnFiNPmXnNjzu');
     let oUfnFiNPmXnNjzu = document.getElementById('CvFHVhLHjFBzNAz');
     let workerID_ = oUfnFiNPmXnNjzu.options[oUfnFiNPmXnNjzu.selectedIndex].value;
     let enter_ = null;
@@ -609,43 +451,5 @@ $('#iHCBwRzOLpgGYQG').on('change', function ()
 
 $(document).ready(function ()
 {
-    //let aFoQOFiXPQobjPX = document.getElementById('aFoQOFiXPQobjPX');
-    //let oUfnFiNPmXnNjzu = document.getElementById('oUfnFiNPmXnNjzu');
-    //let PgLKgYMAvonpLlF = sessionStorage.getItem('PgLKgYMAvonpLlF');
-    //let PgLKgYMAvonpLlF_ = sessionStorage.getItem('PgLKgYMAvonpLlF_');
-    //if (PgLKgYMAvonpLlF != '' && PgLKgYMAvonpLlF_ != '')
-    //{
-    //    //oUfnFiNPmXnNjzu.value = PgLKgYMAvonpLlF;
-
-    //    //console.log(PgLKgYMAvonpLlF_ + ' department');
-    //    //console.log(PgLKgYMAvonpLlF + ' workerID');
-
-    //    aFoQOFiXPQobjPX.value = PgLKgYMAvonpLlF_;
-    //    oUfnFiNPmXnNjzu.value = PgLKgYMAvonpLlF;
-        
-    //}
-    //else
-    //{
-    //    //oUfnFiNPmXnNjzu.value = -1;
-        
-
-    //}
-
-    
-
-
-
-
-
-
-
-
     generateCalendar();
-
-    
 });
-
-//window.onload = function () { 
-//    generateCalendar();
-//    //$('.loader_div').fadeOut(200);
-//};
