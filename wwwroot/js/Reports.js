7d2c5ccacdeb82734d89263ac491a7f7db09efc0
MMattia-dev <kromolski@gmail.com>
2023-12-31 12:03:52 +0100

    1

--- wwwroot/js/Reports.js
+++ wwwroot/js/Reports.js
@@ -1,1 +1,21 @@
 ﻿
+function generateReport1() {
+
+};
+
+function ohGskGVRJUeutBg() {
+    document.getElementById('ozDFgASwhEsiUPZ').innerHTML = '';
+
+    let IrtDSkGZAKrOlBd = document.getElementById('IrtDSkGZAKrOlBd');
+    let departmentID_ = IrtDSkGZAKrOlBd.options[IrtDSkGZAKrOlBd.selectedIndex].value;
+
+    for (let i = 0; i < model_w.length; i++) {
+        if (model_w[i].DepartmentID == departmentID_) {
+            document.getElementById('ozDFgASwhEsiUPZ').innerHTML += `<option value="` + model_w[i].Id + `" id="` + model_w[i].DepartmentID + `">` + model_w[i].Surname + ` ` + model_w[i].Name + `</option>`;
+        }
+    }
+
+    generateReport1();
+};
+ohGskGVRJUeutBg();
+
