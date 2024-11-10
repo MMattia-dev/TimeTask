7af05de7d7c66deafb81f304dc312cc1379b0aa1
MMattia-dev <kromolski@gmail.com>
2024-11-10 15:58:27 +0100

    10.11.2024

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -13,10 +13,10 @@ $(document).on('click', function (event)
 {
     if (!$(event.target).closest('.chatFilter').length)
     {
-        //$('.chatFilter').remove();
+        $('.chatFilter').remove();
 
-        //$('.chatDepartment').removeClass('disabled');
-        //$('.chatMessagesBubbles').removeClass('disabled');
+        $('.chatDepartment').removeClass('disabled');
+        $('.chatMessagesBubbles').removeClass('disabled');
     }
 
     //alwan__palette
@@ -27,16 +27,28 @@ $(document).on('click', function (event)
     const containerElement = document.querySelector('.alwan__container');
 
     if (colorPickerElement) {
-        if (!paletteElement.contains(event.target) || !containerElement.contains(event.target)) //&& !paletteElement.contains(event.target) && !containerElement.contains(event.target)
-        {
-            console.log('asd');
+        //if (!colorPickerElement.contains(event.target)) //&& !paletteElement.contains(event.target) && !containerElement.contains(event.target)
+        //{
+        //    if (!$(event.target).is('body')) {
+        //        console.log('asd');
+        //    }
+            
+            
 
+        //}
+        //else {
+        //    //$('.chatFilter').remove();
 
-        }
-        else {
-            //$('.chatFilter').remove();
+        //}
 
-        }
+        //if ($(event.target).hasClass('alwan__palette') || $(event.target).hasClass('alwan__container'))
+        //{
+        //    console.log('1');
+        //}
+        //else {
+        //    console.log('2');
+        //}
+
     }
     
 
@@ -504,84 +516,89 @@ function chatBackground(loggedUser)
         success: function (response)
         {
             if (response != false) {
-                $('.chatFilter').remove();
-                $('#chat').append(response);
+                //$('.chatFilter').remove();
+                //$('#chat').append(response);
                 $('.chatMessagesBubbles').addClass('disabled');
+
+
+
+
+
 
                 /**/ 
 
-                const alwan = new Alwan('', {
-                    id: 'colorPickerInner',
-                    classname: '',
-                    theme: 'dark',
-                    toggle: false,
-                    popover: false,
-                    position: 'bottom-start',
-                    margin: 8,
-                    preset: false,
-                    color: '#000',
-                    default: '#000',
-                    target: '#colorPicker',
-                    disabled: false,
-                    format: 'rgb',
-                    singleInput: false,
-                    inputs: {
-                        rgb: false,
-                        hex: false,
-                        hsl: false,
-                    },
-                    opacity: false,
-                    preview: true,
-                    closeOnScroll: false,
-                    copy: false,
-                    swatches: [],
-                    toggleSwatches: false,
-                    shared: false,
-                });
-
-                alwan.on('open', function ()
-                {
-                    // do something
-                });
-                alwan.on('close', function ()
-                {
-                    // do something
-                });
-                alwan.on('change', function (colorObject, source)
-                {
-                    // do something
-
-                    //console.log(colorObject);
-                });
-                alwan.on('color', function (colorObject, source)
-                {
-                    // do something
-
-                    //console.log(colorObject);
-                });
-
-                //alwan.on('change', function (color)
-                //{
-                //    // output: { r: 0, g: 0, b: 0, a: 1}
-                //    color.rgb()
-                //    // output: [0, 0, 0, 0]     
-                //    color.rgb(true)
-                //    // output: rgba(0, 0, 0, 1)            
-                //    color.rgb().toString()
-
-                //    // output: #000000
-                //    color.hex()
-
-                //    // output: { h: 0, s: 0, l: 0, a: 1 }
-                //    color.hsl()
-                //    // output: [0, 0, 0, 0]          
-                //    color.hsl(true)
-                //    // output: hsla(0, 0%, 0%, 1)  
-                //    color.hsl().toString()
-
-                //    //console.log(color);
-
+                //const alwan = new Alwan('', {
+                //    id: 'colorPickerInner',
+                //    classname: '',
+                //    theme: 'dark',
+                //    toggle: false,
+                //    popover: false,
+                //    position: 'bottom-start',
+                //    margin: 8,
+                //    preset: false,
+                //    color: '#000',
+                //    default: '#000',
+                //    target: '#colorPicker',
+                //    disabled: false,
+                //    format: 'rgb',
+                //    singleInput: false,
+                //    inputs: {
+                //        rgb: false,
+                //        hex: false,
+                //        hsl: false,
+                //    },
+                //    opacity: false,
+                //    preview: true,
+                //    closeOnScroll: false,
+                //    copy: false,
+                //    swatches: [],
+                //    toggleSwatches: false,
+                //    shared: false,
                 //});
+
+                //alwan.on('open', function ()
+                //{
+                //    // do something
+                //});
+                //alwan.on('close', function ()
+                //{
+                //    // do something
+                //});
+                //alwan.on('change', function (colorObject, source)
+                //{
+                //    // do something
+
+                //    //console.log(colorObject);
+                //});
+                //alwan.on('color', function (colorObject, source)
+                //{
+                //    // do something
+
+                //    //console.log(colorObject);
+                //});
+
+                ////alwan.on('change', function (color)
+                ////{
+                ////    // output: { r: 0, g: 0, b: 0, a: 1}
+                ////    color.rgb()
+                ////    // output: [0, 0, 0, 0]     
+                ////    color.rgb(true)
+                ////    // output: rgba(0, 0, 0, 1)            
+                ////    color.rgb().toString()
+
+                ////    // output: #000000
+                ////    color.hex()
+
+                ////    // output: { h: 0, s: 0, l: 0, a: 1 }
+                ////    color.hsl()
+                ////    // output: [0, 0, 0, 0]          
+                ////    color.hsl(true)
+                ////    // output: hsla(0, 0%, 0%, 1)  
+                ////    color.hsl().toString()
+
+                ////    //console.log(color);
+
+                ////});
 
                 /**/
             }
