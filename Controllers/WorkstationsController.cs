7ecb1816d65b3cbe1f6400e1d30eace1570aa71a
MMattia-dev <kromolski@gmail.com>
2025-01-01 17:06:58 +0100

    01.01.2025

--- Controllers/WorkstationsController.cs
+++ Controllers/WorkstationsController.cs
@@ -162,5 +162,59 @@ namespace TimeTask.Controllers
         {
           return (_context.Workstations?.Any(e => e.Id == id)).GetValueOrDefault();
         }
+
+        [HttpGet]
+        public ActionResult NewWorkstationForm()
+        {
+            var departments = "";
+            foreach (var item in ((IEnumerable<Department>)_context.Department).OrderBy(x => x.Name))
+            {
+                departments += "<option value=" + item.Id + ">" + item.Name + "</option>";
+            }
+
+            string removeForm = "$('#LHalxdaASJOOxrL').remove()";
+
+            string form = "<div id=\"LHalxdaASJOOxrL\" class=\"pGKcZvErUB\" style=\"display: none;\">" +
+                    "<form class=\"form_\">" +
+                        "<div class=\"form-group\">" +
+                            "<label>Dział:</label>" +
+                            "<select class=\"form-control bYwPpsleuVCBkPv\" id=\"OyRfwpeqzbeyVEW\">" +
+                                departments +
+                            "</select>" +
+                        "</div>" +
+                        "<div class=\"form-group form-group-margin\">" +
+                            "<label>Nazwa stanowiska:</label>" +
+                            "<input class=\"form-control\" autocomplete=\"off\" id=\"toPdQnPuvH\" />" +
+                        "</div>" +
+                        "<div class=\"form-group\">" +
+                            "<input type=\"button\" value=\"Zapisz\" class=\"btn-custom\" onclick=\"OwCEPCelyFSecWQ()\" />" +
+                        "</div>" +
+                        "<div class=\"BnDZmDEehCCybzG LPbaczkZTGFbIBk\" onclick=\"" + removeForm + "\">" +
+                            "<svg viewBox=\"0 0 470 470\" height=\"15\" width=\"15\"><path d=\"M310.4,235.083L459.88,85.527c12.545-12.546,12.545-32.972,0-45.671L429.433,9.409c-12.547-12.546-32.971-12.546-45.67,0L234.282,158.967L85.642,10.327c-12.546-12.546-32.972-12.546-45.67,0L9.524,40.774c-12.546,12.546-12.546,32.972,0,45.671l148.64,148.639L9.678,383.495c-12.546,12.546-12.546,32.971,0,45.67l30.447,30.447c12.546,12.546,32.972,12.546,45.67,0l148.487-148.41l148.792,148.793c12.547,12.546,32.973,12.546,45.67,0l30.447-30.447c12.547-12.546,12.547-32.972,0-45.671L310.4,235.083z\"></path></svg>" +
+                        "</div>" +
+                    "</form>" +
+                "</div>";
+
+            return Content(form);
+        }
+
+        [HttpPost]
+        public ActionResult AddNewWorkstation(int departmentId,string name)
+        {
+            var newData = new Workstations()
+            {
+                DepartmentId = departmentId,
+                Name = name
+            };
+
+            _context.Workstations.Add(newData);
+            _context.SaveChanges();
+            return Json(new { success = true });
+        }
+
+
+
+
+
     }
 }
--- wwwroot/js/WorkstationsJS.js
+++ wwwroot/js/WorkstationsJS.js
@@ -1,3 +1,58 @@
 ﻿let selected = document.getElementById('settings_workstations_id');
 selected.classList.add('settings_a_selected');
 
+function xUSjAWmxFhkCiAD()
+{
+    $.ajax({
+        type: 'GET',
+        url: '/Workstations/NewWorkstationForm',
+        success: function (response) 
+        {
+            $('body').append(response);
+            $('#LHalxdaASJOOxrL').fadeIn(200);
+        },
+        error: function (xhr, status, error) 
+        {
+            console.log('Error:', error);
+        }
+    });
+};
+
+function GuDpHEcfHN()
+{
+    if (document.getElementById('toPdQnPuvH').value != '')
+    {
+        let DeViohCQjIFT = document.getElementById('toPdQnPuvH').value;
+
+        OwCEPCelyFSecWQ(DeViohCQjIFT);
+    }
+};
+
+$('#toPdQnPuvH').on('keydown', function (e)
+{
+    if (e.keyCode == '13')
+    {//enter
+        e.preventDefault();
+        GuDpHEcfHN();
+    }
+});
+
+function OwCEPCelyFSecWQ(name_) 
+{
+    $.ajax({
+        type: 'POST',
+        url: '/Workstations/AddNewWorkstation',
+        data: {
+            name: name_,
+        },
+        success: function (response)
+        {
+            //sessionStorage.setItem('qbMvtjjezfxSFsv', response);
+            location.reload();
+        },
+        error: function (xhr, status, error)
+        {
+            console.log('Error:', error);
+        }
+    });
+};
