cf34317c0766c18e8e6b6b3d4c30c79de6a29da9
MMattia-dev <kromolski@gmail.com>
2025-02-20 13:54:24 +0100

    20.02.2025

--- wwwroot/js/OpeningJS.js
+++ wwwroot/js/OpeningJS.js
@@ -147,6 +147,135 @@ function WAknWoEDCgnvjyY(id)
     //}
 };
 
+function resizableGrid(table)
+{
+    var row = table.getElementsByTagName('tr')[0],
+        cols = row ? row.children : undefined;
+    if (!cols) return;
 
+    table.style.overflow = 'hidden';
+
+    var tableHeight = table.offsetHeight;
+
+    for (var i = 0; i < cols.length - 2; i++)
+    {
+        var div = createDiv(tableHeight);
+        cols[i].appendChild(div);
+        cols[i].style.position = 'relative';
+        setListeners(div);
+    }
+
+    function setListeners(div)
+    {
+        var pageX, curCol, nxtCol, curColWidth, nxtColWidth;
+
+        div.addEventListener('mousedown', function (e)
+        {
+            curCol = e.target.parentElement;
+            nxtCol = curCol.nextElementSibling;
+            pageX = e.pageX;
+
+            var padding = paddingDiff(curCol);
+
+            curColWidth = curCol.offsetWidth - padding;
+            if (nxtCol)
+                nxtColWidth = nxtCol.offsetWidth - padding;
+        });
+
+        div.addEventListener('mouseover', function (e)
+        {
+            //e.target.style.borderRight = '4px double rgb(150, 150, 150)';
+        })
+
+        div.addEventListener('mouseout', function (e)
+        {
+            //e.target.style.borderRight = '4px double rgba(255, 255, 255, 0.2)';
+        })
+
+        document.addEventListener('mousemove', function (e)
+        {
+            if (curCol)
+            {
+                var diffX = e.pageX - pageX;
+
+                if (nxtCol)
+                    nxtCol.style.width = (nxtColWidth - (diffX)) + 'px';
+
+                curCol.style.width = (curColWidth + diffX) + 'px';
+            }
+        });
+
+        document.addEventListener('mouseup', function (e)
+        {
+            curCol = undefined;
+            nxtCol = undefined;
+            pageX = undefined;
+            nxtColWidth = undefined;
+            curColWidth = undefined
+        });
+    }
+
+    function createDiv(height)
+    {
+        var div = document.createElement('div');
+        div.style.top = 0;
+        div.style.right = 0;
+        div.style.width = '23px';
+        div.style.zIndex = '1';
+        div.style.position = 'absolute';
+        div.style.cursor = 'col-resize';
+        div.style.userSelect = 'none';
+        div.style.height = '52px';
+
+        div.style.transform = 'translatex(10px)';
+        div.innerHTML += `<i class="arrow left"></i> <div class="lines line1"></div> <div class="lines line2"></div> <i class="arrow right"></i>`;//<div class="lines line1"></div> <div class="lines line2"></div>
+        div.setAttribute('id', 'GRgYMQCkHWKDuyb');
+        div.setAttribute('onmouseover', 'ZKHOrDgJBDHOpmW(this)');
+        div.setAttribute('onmouseout', 'pNxCxvPIUtCbSHM(this)');
+        div.setAttribute('onclick', 'kEDVBzpHnAzOqpp(this, event)');
+
+        return div;
+    }
+
+    function paddingDiff(col)
+    {
+
+        if (getStyleVal(col, 'box-sizing') == 'border-box')
+        {
+            return 0;
+        }
+
+        var padLeft = getStyleVal(col, 'padding-left');
+        var padRight = getStyleVal(col, 'padding-right');
+        return (parseInt(padLeft) + parseInt(padRight));
+
+    }
+
+    function getStyleVal(elm, css)
+    {
+        return (window.getComputedStyle(elm, null).getPropertyValue(css))
+    }
+};
+
+function kEDVBzpHnAzOqpp(t, e)
+{
+    e.stopPropagation();
+};
+
+function ZKHOrDgJBDHOpmW(t)
+{
+    if ($(t).attr('id') == 'GRgYMQCkHWKDuyb')
+    {
+        $(t).parent().addClass('qDIGovQQrGkMAIm');
+    }
+};
+
+function pNxCxvPIUtCbSHM(t)
+{
+    if ($(t).attr('id') == 'GRgYMQCkHWKDuyb')
+    {
+        $(t).parent().removeClass('qDIGovQQrGkMAIm');
+    }
+}
 
 
